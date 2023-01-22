0eb2d20e49891609c65819b148a1c40eb9ed158c
Aadil Fatimi <aHighHooman>
2023-01-21 22:52:37 -0800

    Recommiting

---
+++ Blacksmith.py
@@ -0,0 +1,16 @@
+
+#Test run
+def convo():
+    userInput = input()
+    if(input == 'K_RIGHT'):
+        print("You greet the blacksmith calmly")
+        print("The blacksmith sends a smile your way")
+        print("'Hello there lad, whatcha looking for in this humble smithy.'")
+    elif(input == 'K_UP'):
+        print("You meet the blacksmith with an air of authority")
+        print("The blacksmith frowns looking at you")
+        print("'May I know who you are and what brings you to this crickety shop?'")
+    elif(input == 'K_LEFT'):
+        print("You give a rushed greeting to the blacksmith")
+        print("The blacksmith's eyebrows rise up in curiosity")
+        print("'What's wrong boy, for you to be in such a rush'")
