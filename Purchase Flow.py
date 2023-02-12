da33f4b66c82730f162be9e1b91f8c4353623723
aHighHooman <aHoomanOG@gmail.com>
2023-02-12 00:11:58 -0800

    Organizing

--- Purchase Flow.py
+++ Purchase Flow.py
@@ -1,60 +1,59 @@
 import random as rand
+import time
 
+speed = 0.05
+
+swordList = ["Long Sword", "Broadsword", "Shortsword", "Rapier", "Estoc"]
+
+#utility functions
+def type(input, typeSpeed = speed, speaker = False):
+    if(not speaker):
+        print("- ", end = "", flush = True)
+    for char in input:
+        print(char, end = "", flush = True)
+        time.sleep(typeSpeed)
+    print("\n", end = "", flush = True)
 
 
 #conversational functions 
 def greetCustomer():
     greetings = ["Hello there", "Greetings Customer", "..."]
     num = rand.randint(0,len(greetings)-1)
-    print(greetings[num])
+    type(greetings[num])
 
 def askForNeeds():
     phrases = ["What do you need today","How can I help you today","Whaddya need","Looking for something?"]
     num = rand.randint(0,len(phrases)-1)
-    print(phrases[num])
+    type(phrases[num])
 
 
 #process functions
-def checkWeaponType(list):
-    ### Sword : {Sword,Estoc,Rapier,greatsword}
-    swords = ["sword","estoc","rapier","greatsword"]
-    for i in swords:
-        if(i in list):
-            return i
-    return "none"
-
-def checkSwordType(weaponType,uInput):
-    if(weaponType == "sword"):
-        if ("long" in uInput):
-            return "long sword"
-        elif("short" in uInput):
-            return "short"
-        elif("broad" in uInput):
-            return "broad sword"
-        else:
-            return "sword"
-    elif(weaponType == "rapier"):
-        return weaponType
-    elif(weaponType == "estoc"):
-        return weaponType
+def checkAvailability(ID):
+    if (ID == 6):
+        type("Unfortunately I do not have any other selections right now.")
     else:
-        return "none"
+        type(f"Oh I have a {swordList[ID]}")
     
+def showcaseWeapon(ID):
+    if (ID == 1 or ID == 2 or ID == 3):
+        type(f"This {swordList[ID]} will cost 5 gold.")
+    elif(ID == 4):
+        type("This was on of my favorite ones to work on... it costs 8 gold.")
+    elif(ID == 5):
+        type(f"The {swordList[ID]} costs 7 gold")
 
 
 #main
 greetCustomer() #Greet the player can change depending on the mood
 askForNeeds()
 
-val = input("I need a: ")
-userInput = val.split(" ")
+type("I need a: \n1: Long Sword \n2: Broadsword\n3: Shortsword\n4: Rapier\n5: Estoc\n6: Other", typeSpeed=0.01, speaker=True)
+val = int(input())
+while(val > 6 or val < 1):
+    val = int(input("I need a: \n1. Long Sword \n2. Broadsword\n3. Shortsword\n4. Rapier\n5. Estoc\n6. Other"))
 
-if(len(userInput) <= 2):
-    weaponType = checkWeaponType(userInput)
-    swordType = checkSwordType(weaponType,userInput)
-    if(swordType == "none"):
-            print(f"Unfortunately I do not have a {val}")
-    else:
-            print(f"Oh I do have a {val}")
-else:
-    print(f"Unfortunately I do not have a '{val}'")
+checkAvailability(val-1)
+if(not(val == 6)):
+    showcaseWeapon(val-1)
+#else:
+    #Apoligies of sorts
